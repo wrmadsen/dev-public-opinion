6f208dcb78fac1ec91b1e0603923e60bea1f762a
willwrm <55885264+willwrm@users.noreply.github.com>
2021-02-05 18:42:53 +0100

    get tweets and save as CSVs for each day

--- src/py/get_tweets.py
+++ src/py/get_tweets.py
@@ -1,18 +1,27 @@
 # Get tweets
 import twint
-import os
+#import os
 
 # Wd
 wd = "/Users/williamrohdemadsen/Dropbox/UCL/Year 3/Term 2/POLS0014 Diss/dev-public-opinion"
 
-# Configure
-c = twint.Config()
-c.Search = "buhari"
-c.Lang = "en"
-c.Geo = "6.45506,3.39418,5km" # lagos
-c.Limit = 300
-c.Output = wd + "/data/tweets/buhari.csv"
-c.Store_json = True
+# Create function to get tweets
+def get_tweets(search, lang, geo, limit, since, until, csv_path):
 
-# Run
-twint.run.Search(c)
+    # Arguments
+    c = twint.Config()
+    c.Search = search
+    c.Lang = lang
+    c.Geo = geo # lagos 6.45506,3.39418,5km
+    c.Limit = limit
+    c.Since = since
+    c.Until = until
+    c.Output = csv_path
+    c.Store_json = True
+
+    # Run
+    twint.run.Search(c)
+
+#get_tweets("buhari", "en", "6.45506,3.39418,5km", 300)
+
+#get_tweets("frederiksen", "en", "55.66666667,12.583333,5km",  10, "2020-04-05",  "2020-04-06", wd)
