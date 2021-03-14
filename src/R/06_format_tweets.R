9bcd312378159f71b266d349f6c9fd836386ccaa
willwrm <55885264+willwrm@users.noreply.github.com>
2021-03-14 18:59:05 +0100

    format tweets

--- src/R/06_format_tweets.R
+++ src/R/06_format_tweets.R
@@ -1,20 +1,17 @@
-###### Format tweets
+# Format tweets
 
 # Tidy and format tweets ------
 tweets <- tweets_raw %>%
-  # select(contains("hashtags.")) %>%
-  # view()
-  unite(col = "hashtags_", contains("hashtags."), na.rm = TRUE) %>%
-  unite(col = "photos_", contains("photos."), na.rm = TRUE) %>%
-  unite(col = "urls_", contains("urls."), na.rm = TRUE) %>%
-  relocate(matches("//d|0"), .after = last_col()) %>%
-  mutate(filename,
-         date = as.Date(date),
-         country = gsub(".*_(.*)_.*", "\\1", filename),
-         leader = gsub(paste0(".*//(.*)_", country, ".*"), "\\1", filename)
-  ) %>%
   clean_names() %>%
-  select(date, name, country, leader, place, everything(),-c(contains("mentions"), contains("reply"), "filename"))
+  # unite(col = "hashtags_", contains("hashtags."), na.rm = TRUE) %>%
+  # unite(col = "photos_", contains("photos."), na.rm = TRUE) %>%
+  # unite(col = "urls_", contains("urls."), na.rm = TRUE) %>%
+  # relocate(matches("//d|0"), .after = last_col()) %>%
+  mutate(date = as.Date(date),
+         leader = gsub("data.+/|_.+", "", filename)
+  ) %>%
+  left_join()
+  select(id, username, name, date, country, leader, place)
 
 tweets
 
