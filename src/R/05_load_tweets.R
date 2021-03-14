78ccc910c4dd5a7412f0204cdf0d1ec0c1b7cf23
willwrm <55885264+willwrm@users.noreply.github.com>
2021-03-14 18:02:04 +0100

    paste place variable in loading back tweets

--- src/R/05_load_tweets.R
+++ src/R/05_load_tweets.R
@@ -2,13 +2,14 @@
 
 # Load all Tweets ------
 # Circa size of Tweets
-file.size("data/tweets/nigeria")/1000 # mb
+file.size("data/tweets/without/")/1000 # mb
 
 # Function to read in and add file name as column
 read_tweets_back <- function(json){
   
   df <- stream_in(file(json)) %>%
-    as_tibble()
+    as_tibble() %>%
+    mutate(place = paste0(place))
   
   df$filename <- json
   
@@ -16,15 +17,6 @@ read_tweets_back <- function(json){
   
 }
 
-#read_tweets_back("data/tweets/Buhari2018-01-03.json")
-
 # Map across function load all Tweets
 tweets_raw <- list.files("data/tweets", full.names = TRUE, recursive = TRUE) %>%
   map_df(~read_tweets_back(.))
-
-
-stream_in("data/tweets/Nigeria/Buhari_Bauchi_Bauchi_2019-01-12.json") %>% as_tibble()
-
-fromJSON(paste(readLines("data/tweets/Nigeria/Buhari_Bauchi_Bauchi_2019-01-12.json"), collapse = ""))
-
-stream_in(file("data/tweets/Nigeria/Buhari_Bauchi_Bauchi_2019-01-12.json")) %>% view
