b90d7bd2b24b62a4995c35ede482194a74676141
willwrm <55885264+willwrm@users.noreply.github.com>
2021-03-14 20:35:23 +0100

    add initial sentiment analysis with afinn lexicon

--- src/R/07_sentiment_analysis.R
+++ src/R/07_sentiment_analysis.R
@@ -6,14 +6,32 @@ names(tweets)
 
 # Subset
 tweets_corpus <- tweets %>%
-  mutate(leader = gsub("data.+/|_.+", "", tweets_raw$filename)) %>%
-  select(id, username, tweet, leader)
+  select(id, username, date, leader, country, tweet)
 
 # Head of corpus
 head(tweets_corpus)
 
+# Tidytext format ------
 # Convert to tidytext format
 tweets_tidy <- tweets_corpus %>%
   unnest_tokens(word, tweet)
 
+# Run sentiment analysis ----
+afinn <- get_sentiments("afinn")
 
+tweets_senti <- tweets_tidy %>%
+  left_join(afinn)
+
+# Binary classification -------
+tweets_binary <- tweets_senti %>%
+  group_by(id, username, leader, country, date) %>%
+  summarise(mean = mean(value, na.rm = TRUE)) %>%
+  mutate(binary = if_else(mean > 0, 1, 0))
+
+# Plot -----
+tweets_binary %>%
+  group_by(date, leader, country, binary) %>%
+  summarise(count = n())
+  ggplot(.,
+       aes(x = date,
+           y = ))
