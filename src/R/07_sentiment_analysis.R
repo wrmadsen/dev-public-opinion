7ab7c8a58f7b11bb39f84d953ed057462fe651e5
willwrm <55885264+willwrm@users.noreply.github.com>
2021-03-14 18:58:58 +0100

    convert to tidytext format

--- src/R/07_sentiment_analysis.R
+++ src/R/07_sentiment_analysis.R
@@ -1,5 +1,19 @@
 # Sentiment analysis
 
 # Clean tweets ----
-tweets_raw
+# Print raw corpus columns
+names(tweets)
+
+# Subset
+tweets_corpus <- tweets %>%
+  mutate(leader = gsub("data.+/|_.+", "", tweets_raw$filename)) %>%
+  select(id, username, tweet, leader)
+
+# Head of corpus
+head(tweets_corpus)
+
+# Convert to tidytext format
+tweets_tidy <- tweets_corpus %>%
+  unnest_tokens(word, tweet)
+
 
