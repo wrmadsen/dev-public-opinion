2152ee892de66eef60ad949c9c85dea26e10bef5
willwrm <55885264+willwrm@users.noreply.github.com>
2021-03-16 18:04:05 +0100

    use flatten() to load in tweets before binding to a single df

---
+++ src/R/05_load_tweets.R
@@ -0,0 +1,14 @@
+###### Load tweets
+
+# Function to read in and add file name as column
+read_tweets_back <- function(json){
+  
+  df <- stream_in(file(json)) %>%
+    flatten() %>%
+    as_tibble()
+  
+  df$filename <- json
+  
+  df
+  
+}
