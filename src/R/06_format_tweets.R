f540194aa72080512885f70c8ea4a50d4adf7cfc
willwrm <55885264+willwrm@users.noreply.github.com>
2021-03-16 20:18:18 +0100

    function to format and subset tweets raw

--- src/R/06_format_tweets.R
+++ src/R/06_format_tweets.R
@@ -1,23 +1,33 @@
 # Format tweets
 
 # Tidy and format tweets ------
-tweets <- tweets_raw %>%
-  clean_names() %>%
-  # unite(col = "hashtags_", contains("hashtags."), na.rm = TRUE) %>%
-  # unite(col = "photos_", contains("photos."), na.rm = TRUE) %>%
-  # unite(col = "urls_", contains("urls."), na.rm = TRUE) %>%
-  # relocate(matches("//d|0"), .after = last_col()) %>%
-  mutate(date = as.Date(date),
-         week = floor_date(date, "week"),
-         leader = gsub("data.+/|_.+", "", filename),
-  ) %>%
-  left_join(., 
-            distinct(candidates, country, name),
-            by = c("leader" = "name")) %>%
-  select(id, username, name, date, week, country, leader, place, tweet)
+format_tweets <- function(tweets_raw, candidates){
+  
+  # Look-up to add leaders and countries
+  candidates <- distinct(candidates, country, name)
+  
+  # Format long and lat
+  tweets <- tweets_raw %>%
+    mutate(x = purrr::map(place.coordinates, 1) %>% paste %>% as.double,
+           y = purrr::map(place.coordinates, 2) %>% paste %>% as.double
+    )
+  
+  # Clean and select variables
+  tweets %>%
+    clean_names() %>%
+    # unite(col = "hashtags_", contains("hashtags."), na.rm = TRUE) %>%
+    # unite(col = "photos_", contains("photos."), na.rm = TRUE) %>%
+    # unite(col = "urls_", contains("urls."), na.rm = TRUE) %>%
+    # relocate(matches("//d|0"), .after = last_col()) %>%
+    mutate(date = as.Date(date),
+           week = floor_date(date, "week"),
+           leader = gsub("data.+/|_.+", "", filename),
+    ) %>%
+    left_join(., 
+              candidates,
+              by = c("leader" = "name")) %>%
+    select(id, username, name, date, week, country, leader,
+           x, y, language, tweet)
+  
+}
 
-# Print head
-head(tweets)
-
-# Dimensions
-dim(tweets)
