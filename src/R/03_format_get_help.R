14e208b43bd8e9df6bc337499f680d2e18995a28
willwrm <55885264+willwrm@users.noreply.github.com>
2021-03-16 16:02:53 +0100

    convert get data steps into functions

--- src/R/03_format_get_help.R
+++ src/R/03_format_get_help.R
@@ -29,68 +29,72 @@ create_scrape_data <- function(reign, candidates_scrape, days){
               date_end = date + days(days-1), # scrape end time (to)
     ) %>%
     filter(date >= as.Date("2006-07-15")) # when Twitter full version went live
-
-    
+  
+  
 }
 
 # Create smallest possible circles
 create_smallest_possible <- function(gadm){
   
-  # Turn to owin
+  # For loop to find circle for each country
+  small_circs <- c()
+  
+  for (i in 1:nrow(gadm)){
+    
+    # Create smallest-possible circle
+    small_circ <- gadm[i,] %>%
+      st_transform(3857) %>% # need planar projection
+      as_Spatial() %>%
+      as.owin() %>% # convert to owin to use boundingcircle()
+      boundingcircle() %>%
+      st_as_sf() %>%
+      st_set_crs(3857)
+    
+    # Add to vector
+    small_circs[i] <- small_circ$geom
+    
+  }
+  
+  # Add to GADM to include country and id variables 
   gadm %>%
-    st_transform(3857) %>% # need planar projection
-    as_Spatial() %>%
-    as.owin() %>%
-    boundingcircle() %>%
-    st_as_sf() %>%
-    st_set_crs(3857)
+    as_tibble() %>%
+    transmute(id = row_number(), country, geom = small_circs) %>%
+    st_as_sf(crs = 3857) %>%
+    st_transform(4326)
   
 }
 
-# Add smallest-possible circles (spc) to dataframe
-add_smallest_possible <- function(gadm, circs)
-
-  gadm %>%
-  as.data.frame() %>%
-  mutate(id = row_number()) %>%
-  select(-geometry) %>%
-  st_set_geometry(., st_sfc(circs, crs = 3857)) %>%
-  st_transform(4326)
-
+# Find centre and radius
+find_centre_and_radius <- function(small_circs){
+  
+  # Find centres in BNG
+  small_centres <- small_circs %>%
+    st_transform(., 27700) %>% # convert to British National Grid to work in meters, required for calculating radius
+    st_centroid()
+  
+  # Convert circles to linestring before finding radius
+  small_lines <- small_circs %>%
+    st_transform(., 27700) %>% # convert to British National Grid to work in meters, required for the scraping radius
+    st_cast(to = "MULTILINESTRING")
+  
+  # Find and add radius in metres
+  small_circs$radius_m <- st_distance(small_lines$geometry, small_centres$geometry,
+                                      by_element = TRUE,
+                                      which = "Euclidean"
+  )
+  
+  # Add centres in 4326 to object
+  small_centres_4326 <- small_centres %>%
+    st_transform(4326) %>%
+    st_coordinates() %>%
+    as_tibble() %>%
+    transmute(x = X,
+              y = Y,
+              id = row_number())
+  
+  # Join
+  small_circs %>%
+    left_join(small_centres_4326, by = "id", keep = FALSE) %>%
+    as_tibble()
+  
 }
-
-# Find centre and calculate radius
-small_centres <- small_circs %>%
-  st_transform(., 27700) %>% # convert to British National Grid to work in meters, required for calculating radius
-  st_centroid()
-
-# Convert to linestring before calculating distance
-small_lines <- small_circs %>%
-  st_transform(., 27700) %>% # convert to British National Grid to work in meters, required for the scraping radius
-  st_cast(to = "MULTILINESTRING")
-
-# Calculate radius in metres
-small_circs$radius <- st_distance(small_lines$geometry, small_centres$geometry,
-                                  by_element = TRUE,
-                                  which = "Euclidean"
-)
-
-# Get coordinates of centres in longitude and latitude before joining
-small_centres_4326 <- small_centres %>%
-  st_transform(4326) %>%
-  st_coordinates() %>%
-  as_tibble() %>%
-  transmute(x = X,
-            y = Y,
-            id = row_number())
-
-# Add centre points to dataset
-country_geocode <- small_circs %>%
-  left_join(small_centres_4326, by = "id", keep = FALSE) %>%
-  as_tibble() %>%
-  transmute(country, geocode = paste0(x, ",", y, ",", radius/1000, "km"))
-
-# Join data -----
-scrape_data <- names_scrape %>%
-  left_join(country_geocode, by = "country") %>%
-  arrange(name, desc(date)) # descending to get recent tweets first
