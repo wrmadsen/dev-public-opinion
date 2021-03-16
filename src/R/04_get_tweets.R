723335c7bf4e4a4b6b7e87a507478af1cb41cc56
willwrm <55885264+willwrm@users.noreply.github.com>
2021-03-16 22:18:15 +0100

    fix limit and geocode params

--- src/R/04_get_tweets.R
+++ src/R/04_get_tweets.R
@@ -1,25 +1,26 @@
 ###### Get Tweets with Python's twint
 
 # Get tweets without point data -------
-get_tweets_r <- function(scrape_data, path = "data-raw/tweets/without/", limit, geocode = ""){
+get_tweets_r <- function(scrape_data, path = "data-raw/tweets/without/", limit = FALSE, include_geocode = FALSE){
   
   scrape_data %>%
     mutate(file_path = paste0(path, name, "_", paste(date), ".json"), # name of file to be saved
            date = paste0(date, " 00:00:00"),
-           date_end = paste0(date_end, " 23:59:59")
+           date_end = paste0(date_end, " 23:59:59"),
+           include_geocode = include_geocode,
+           geocode = if_else(include_geocode, geocode, ""),
+    ) %>%
+    mutate(tweets = pmap(list(name, geocode, date, date_end, file_path),
+                         ~get_tweets(..1,
+                                     "en",
+                                     ..2, # no geocode
+                                     FALSE, # limit
+                                     ..3,
+                                     ..4,
+                                     ..5
+                         )
     )
-  
-    # mutate(tweets = pmap(list(name, date, date_end, file_path),
-    #                      ~get_tweets(..1,
-    #                                  "en",
-    #                                  geocode, # no geocode
-    #                                  limit, # limit
-    #                                  ..2,
-    #                                  ..3,
-    #                                  ..4
-    #                      )
-    # )
-    # )
+    )
   
 }
 
