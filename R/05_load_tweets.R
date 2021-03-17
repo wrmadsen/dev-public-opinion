c6818ee75d7184b6e8e81589b2aaf1e5bd25e3ab
willwrm <55885264+willwrm@users.noreply.github.com>
2021-03-17 21:48:24 +0100

    add comments

--- R/05_load_tweets.R
+++ R/05_load_tweets.R
@@ -1,26 +1,29 @@
-# Load tweets
+#' Read raw tweets from JSONs
+#'
+#' @param json_path path to folder with JSON files of raw tweets
+#' @return List of raw tweets tibbles
+read_tweets_back <- function(json_path){
 
-# Function to read in and add file name as column
-read_tweets_back <- function(json){
-  
-  tweets_raw <- stream_in(file(json)) %>%
+  tweets_raw <- jsonlite::stream_in(file(json_path)) %>%
     flatten() %>%
     as_tibble()
-  
-  tweets_raw$filename <- json
-  
+
+  tweets_raw$filename <- json_path
+
   tweets_raw
-  
+
 }
 
-
-# Bindlists of raw tweets
+#' Bind raw tweets into tibble
+#'
+#' @param tweets_raw List of raw tweets tibbles
+#' @return Single tibble of raw tweets
 bind_raw_tweets <- function(tweets_raw){
-  
+
   # Use map to clean before binding tibbles in a list
   tweets_raw %>%
-    purrr::map(~mutate(., place = "") # need to add place to 
+    purrr::map(~mutate(., place = "") # need to add place to
     ) %>%
     bind_rows()
-  
+
 }
