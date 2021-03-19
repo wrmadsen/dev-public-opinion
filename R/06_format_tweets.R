bbf8e10f07a1a60df6184af47fd31aa246e1e325
willwrm <55885264+willwrm@users.noreply.github.com>
2021-03-19 22:02:11 +0100

    add function to find region name for tweet

--- R/06_format_tweets.R
+++ R/06_format_tweets.R
@@ -6,7 +6,7 @@
 format_tweets <- function(tweets_raw, candidates){
 
   # Look-up to add leaders and countries
-  candidates <- distinct(candidates, country, name)
+  candidates_lookup <- distinct(candidates, country, name)
 
   # Format long and lat
   tweets <- tweets_raw %>%
@@ -15,21 +15,51 @@ format_tweets <- function(tweets_raw, candidates){
     )
 
   # Clean and select variables
-  tweets %>%
+  tweets_formatted <- tweets %>%
     clean_names() %>%
-    # unite(col = "hashtags_", contains("hashtags."), na.rm = TRUE) %>%
-    # unite(col = "photos_", contains("photos."), na.rm = TRUE) %>%
-    # unite(col = "urls_", contains("urls."), na.rm = TRUE) %>%
-    # relocate(matches("//d|0"), .after = last_col()) %>%
     mutate(date = as.Date(date),
            week = floor_date(date, "week"),
            leader = gsub("data.+/|_.+", "", filename),
     ) %>%
     left_join(.,
-              candidates,
+              candidates_lookup,
               by = c("leader" = "name")) %>%
     select(id, username, name, date, week, country, leader,
            x, y, language, tweet)
 
+  tweets_formatted
+
 }
 
+#' Add region variable to users who shared their point data
+#'
+#' @param tweets_formatted
+#' @param boundaries_subnational
+#' @return formatted tweets tibble with region names for some users
+add_regions <- function(tweets_formatted, boundaries_subnational){
+
+  # Convert to tweets with points to sf
+  tweets_points <- tweets_formatted %>%
+    filter(!is.na(x)) %>%
+    st_as_sf(., coords = c("x", "y"), crs = 4326)
+
+  # Find intersections between points and polygons
+  intersections <- st_intersects(tweets_points, boundaries_subnational)
+
+  # Create region-row-number look-up
+  region_look_up <- boundaries_subnational %>%
+    as.data.frame() %>%
+    transmute(region_id = row_number(),
+              region_1)
+
+  # Add regions and bind non-point tweets back together
+  tweets_points %>%
+    mutate(region_id = as.integer(intersections)
+           ) %>%
+    left_join(., region_look_up, by = "region_id") %>%
+    bind_rows(tweets_formatted %>%
+                filter(is.na(x)) %>%
+                select(-c(x, y))
+              )
+
+}
