909f01fde56ce95a6c43cebbf8ae2e0b4a5b22d3
willwrm <55885264+willwrm@users.noreply.github.com>
2021-03-24 14:05:55 +0100

    join raw estimates to election results by day, country and region_1

--- R/validation.R
+++ R/validation.R
@@ -1,30 +1,27 @@
 # Validation
 
 #' Join election results to raw sentiment estimates
-#' @param senti_pro_shares
+#' @param raw_pro_shares
 #' @param elex_master
-
-# Need to add join by area as well, or option to do so
-join_election_to_raw_senti <- function(senti_pro_shares, elex_master){
+join_election_to_raw_senti <- function(raw_pro_shares, elex_master){
 
   # Join election results to pro shares
-  validate_raw <- senti_pro_shares %>%
-    mutate(id = row_number()) %>%
+  validate_elex_raw <- raw_pro_shares %>%
+    mutate(region_1 = if_else(is.na(region_1), "National", region_1),
+           id = row_number()) %>%
     left_join(., elex_master,
-              by = c("country", "leader" = "name", "region_1" = "area"))
+              by = c("country", "leader" = "name", "region_1"))
 
   # For each tweet, select the nearest election in time
-  validate_raw <- validate_raw %>%
+  # One tweet, one election
+  validate_elex_raw %>%
     group_by(country, leader, cut_off, pro_share, id) %>%
     # diff between tweet and election date in days
-    mutate(date_diff = (elex_date - week),
-           date_diff = as.integer(date_diff) %>% abs) %>%
-    slice_min(diff, n = 1) %>%
-    ungroup()
-
-
-  validate_raw
-
-
+    mutate(days_diff = (elex_date - date) %>% as.integer,
+           days_diff_abs = abs(days_diff)
+           ) %>%
+    slice_min(days_diff_abs, n = 1) %>%
+    ungroup() %>%
+    arrange(country, leader, region_1, date)
 
 }
