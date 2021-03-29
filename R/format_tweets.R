787b6817c5e609201b6f240a10a73eff2b57f7fa
William Rohde Madsen <wrohdemadsen@icloud.com>
2021-03-30 01:51:56 +0200

    remove functions that relied on reticulate, cannot use multiprocessing

--- R/format_tweets.R
+++ R/format_tweets.R
@@ -5,72 +5,40 @@
 #' @param limit
 #' @param include_geocode
 #' @return Raw tweets saved to path in a JSON per period
-get_tweets_per <- function(scrape_data, limit = 1000000, include_geocode = FALSE){
-
-  # Subset based on path and geocode
-  scrape_data_sub <- scrape_data %>%
-    mutate(include_geocode = include_geocode,
-           file_path = if_else(include_geocode,
-                               paste0("data-raw/tweets/with/", name, "_", paste(date), ".json"),
-                               paste0("data-raw/tweets/without/", name, "_", paste(date), ".json")
-           ),
-           date = paste0(date, " 00:00:00"),
-           date_end = paste0(date_end, " 23:59:59"),
-           geocode = if_else(include_geocode, geocode, ""),
-    ) %>%
-    transmute(search = name,
-              geo = geocode,
-              limit = limit,
-              since = date,
-              until = date_end,
-              path = file_path
-    ) #%>% view
-
-  # Set up parallel queries
-  no_cores <- availableCores() - 2
-  #plan(cluster)
-  #plan(sequential)
-  plan(multicore, workers = no_cores)
-
-  # Map with future
-  future_pwalk(scrape_data_sub, get_tweets, .progress = TRUE)
-  #future_pmap(scrape_data_sub, get_tweets, .progress = TRUE)
-
-}
-
-#' Get tweets total
-#'
-#' @param scrape_data
-#' @param limit
-#' @param include_geocode
-#' @return Raw tweets saved to path in a JSON per period
-get_tweets_total <- function(scrape_data, limit = 1000000000, include_geocode = FALSE){
-
-  # Subset based on path and geocode
-  scrape_data_sub <- scrape_data %>%
-    group_by(country, name, geocode) %>%
-    summarise(date = min(date),
-              date_end = max(date_end),
-              include_geocode = include_geocode
-    ) %>%
-    ungroup() %>%
-    transmute(search = name,
-              geo = if_else(include_geocode, geocode, ""),
-              limit = limit,
-              since = paste0(date, " 00:00:00"),
-              until = paste0(date_end, " 23:59:59"),
-              path = if_else(include_geocode,
-                             paste0("data-raw/tweets/total/", name, "_",
-                                    paste(date), "_to_", paste(date_end), "_with.json"),
-                             paste0("data-raw/tweets/total/", name, "_",
-                                    paste(date), "_to_", paste(date_end), "_without.json")
-              )
-    ) #%>% view
-
-  # Map across Python function
-  pmap(scrape_data_sub, get_tweets)
-
-}
+# get_tweets_per <- function(scrape_data, limit = 1000000, include_geocode = FALSE){
+#
+#   # Subset based on path and geocode
+#   scrape_data_sub <- scrape_data %>%
+#     mutate(include_geocode = include_geocode,
+#            file_path = if_else(include_geocode,
+#                                paste0("data-raw/tweets/with/", name, "_", paste(date), ".json"),
+#                                paste0("data-raw/tweets/without/", name, "_", paste(date), ".json")
+#            ),
+#            date = paste0(date, " 00:00:00"),
+#            date_end = paste0(date_end, " 23:59:59"),
+#            geocode = if_else(include_geocode, geocode, ""),
+#     ) %>%
+#     transmute(search = name,
+#               geo = geocode,
+#               limit = limit,
+#               since = date,
+#               until = date_end,
+#               path = file_path
+#     ) #%>% view
+#
+#   # Set up parallel queries
+#   no_cores <- availableCores() - 1
+#   #plan(cluster)
+#   #plan(sequential)
+#   future::plan(multicore)
+#
+#   # Map with future
+#   future_pwalk(scrape_data_sub, get_tweets,
+#                .options = furrr_options(lazy = FALSE,
+#                                         scheduling = 1,
+#                                         chunk_size = 1))
+#
+# }
 
 
 # Load tweets ----
