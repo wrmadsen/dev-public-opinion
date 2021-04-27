13cf6bbefba5d58059230e67f6fbc45dc63f831c
William Rohde Madsen <wrohdemadsen@icloud.com>
2021-04-27 22:04:36 +0100

    filter duplicates and NA leaders, capitalise leaders

--- R/format_tweets.R
+++ R/format_tweets.R
@@ -81,46 +81,94 @@ bind_raw_tweets <- function(tweets_raw){
 format_tweets <- function(tweets_raw, candidates){
 
   # Look-up to add leaders and countries
-  candidates_lookup <- distinct(candidates, country, name)
+  candidates_lookup <- candidates %>%
+    distinct(country, name) %>%
+    transmute(name = name,
+              name_lower = tolower(name),
+              leader_country = country)
 
-  # Format long and lat
-  tweets <- tweets_raw %>%
-    mutate(x = purrr::map(place.coordinates, 1) %>% paste %>% as.double,
-           y = purrr::map(place.coordinates, 2) %>% paste %>% as.double
+  candidates_match <- paste(candidates_lookup$name_lower, collapse = "|")
+
+  # Get long and lat coordinates
+  tweets_coords <- tweets_raw %>%
+    mutate(x = str_match(place, "\\[(.*?),")[,2] %>% as.double(),
+           y = str_match(place, "\\d, (.*?)\\]")[,2] %>% as.double()
     )
 
+  # Find which leader(s) is/are mentioned in tweet
+  tweets_leader <- tweets_coords %>%
+    mutate(leader_mentions = str_extract_all(tolower(tweet), candidates_match)) %>%
+    rowwise() %>%
+    mutate(leader_unique = length(unique(leader_mentions))) %>%
+    ungroup()
+
+  # Add leader name if single
+  tweets_leader <- tweets_leader %>%
+    mutate(leader = if_else(leader_unique == 1,
+                            purrr::map(leader_mentions, 1) %>% paste,
+                            NA_character_)
+           )
+
+  # Add back capitalised leader names
+  tweets_cap <- tweets_leader %>%
+    left_join(.,
+              candidates_lookup %>% select(-leader_country) %>% rename(leader = name),
+              by = c("leader" = "name_lower")
+              ) %>%
+    # USe capitalised leader names
+    select(-leader) %>%
+    rename(leader = leader.y)
+
   # Clean and select variables
-  tweets_formatted <- tweets %>%
+  tweets_formatted <- tweets_cap %>%
     clean_names() %>%
     mutate(date = as.Date(date),
            week = floor_date(date, "week"),
-           leader = gsub("data.+/|_.+", "", filename),
-    ) %>% view
+    ) %>%
     left_join(.,
               candidates_lookup,
-              by = c("leader" = "name")) %>%
-    select(id, username, name, date, country, leader,
+              by = c("leader" = "name")
+              ) %>%
+    select(id, conversation_id, username, name, date, week, leader_country, leader, leader_unique, leader_mentions,
            x, y, language, tweet, replies_count, retweets_count, likes_count)
 
   tweets_formatted
 
 }
 
+#' Filter tweets
+filter_tweets <- function(tweets_formatted){
+
+  # Remove duplicates by choosing the duplicate with most likes
+  tweets_no_dups <- tweets_formatted %>%
+    group_by(id, conversation_id) %>%
+    slice_max(likes_count, n = 1, with_ties = FALSE) %>% # for duplicates, choose the one with most likes
+    ungroup()
+
+  # Remove tweets that don't mention a single leader
+  tweets_no_dups %>%
+    filter(!is.na(leader))
+
+}
+
+
 #' Add region variable to users who shared their point data
 #'
 #' @param tweets_formatted
 #' @param boundaries_subnational
 #' @return formatted tweets tibble with region names for some users
-add_regions <- function(tweets_formatted, boundaries_subnational){
+add_regions <- function(tweets_sub, boundaries_subnational){
 
   # Convert to tweets with points to sf
-  tweets_points <- tweets_formatted %>%
+  tweets_points <- tweets_sub %>%
     filter(!is.na(x)) %>%
-    st_as_sf(., coords = c("y", "x"), crs = 4326)
+    st_as_sf(., coords = c("y", "x"), crs = 4326) %>%
+    mutate(has_point = TRUE)
 
-  tweets_no_points <- tweets_formatted %>%
+  tweets_no_points <- tweets_sub %>%
     filter(is.na(x)) %>%
-    select(-c(x, y))
+    select(-c(x, y)) %>%
+    mutate(has_point = FALSE)
 
   # Find intersections between points and polygons
   intersections <- st_intersects(tweets_points, boundaries_subnational)
@@ -129,7 +177,12 @@ add_regions <- function(tweets_formatted, boundaries_subnational){
   region_look_up <- boundaries_subnational %>%
     as.data.frame() %>%
     transmute(region_id = row_number(),
-              region_1)
+              country,
+              region_1,
+              engtype_1,
+              region_2,
+              engtype_2
+              )
 
   # Add regions to tweets with points
   tweets_w_regions <- tweets_points %>%
@@ -144,8 +197,8 @@ add_regions <- function(tweets_formatted, boundaries_subnational){
 
   } else {
 
-  tweets_w_regions %>%
-    bind_rows(tweets_no_points)
+    tweets_w_regions %>%
+      bind_rows(tweets_no_points)
 
   }
 
