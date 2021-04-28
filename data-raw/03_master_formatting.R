06895ad97c5d4adfb3582cd6de766aa7700a406f
William Rohde Madsen <wrohdemadsen@icloud.com>
2021-04-28 19:26:15 +0100

    create separate code for getting tweets with geocode

--- data-raw/03_master_formatting.R
+++ data-raw/03_master_formatting.R
@@ -15,6 +15,7 @@ library(reticulate)
 library(future)
 library(furrr)
 library(textdata)
+library(haven)
 
 # Source R functions
 list.files("R", full.names = TRUE) %>% purrr::map(source)
@@ -25,11 +26,37 @@ source("data-raw/01_load_raw.R")
 # Format raw data -----
 source("data-raw/02_format_raw_data.R")
 
-# Get and format tweets -----
+# Helper data for getting tweets -----
 
+## All ----
 # Create object with get frequency and names
 # People names must match between reign and people_to_get
-get_names <- create_get_freq(reign, candidates, by_type = "days", by_n = 3)
+get_names_all <- create_get_freq(reign, candidates, by_type = "days", by_n = 5)
+
+# Join data
+get_data_all <- get_names %>%
+  left_join(geocodes, by = "country") %>%
+  filter(!is.na(geocode)) %>%
+  arrange(name, desc(date)) # descending to get recent tweets first
+
+# Save get data as csv
+# Multiprocessing in Python cut time from 150 secs to 22 secs (14x as fast)
+(get_data_all_csv <- get_data_all %>%
+    filter(country %in% c("Georgia", "Zimbabwe", "Mexico")) %>%
+    transmute(leader = name,
+              date = paste(date),
+              date_end = paste(date_end),
+              row_no = paste0(row_number(), "/", n())
+    )
+)
+
+get_data_all_csv$leader %>% unique
+
+write_csv(get_data_all_csv, "py/get_data_all.csv")
+
+## With geocodes ----
+# Frequency for collecting tweets with geocodes can be larger to save time
+get_names_geo <- create_get_freq(reign, candidates, by_type = "days", by_n = 200)
 
 # Create smallest possible circles
 small_circs <- create_smallest_possible(boundaries_national)
@@ -41,45 +68,42 @@ centre_and_radius <- find_centre_and_radius(small_circs)
 geocodes <- centre_and_radius %>%
   transmute(country, geocode = paste0(x, ",", y, ",", radius_m/1000, "km"))
 
-# Join data
-get_data <- get_names %>%
+# Join geocodes and arrange data
+get_data_geo <- get_names_geo %>%
   left_join(geocodes, by = "country") %>%
   filter(!is.na(geocode)) %>%
   arrange(name, desc(date)) # descending to get recent tweets first
 
 # Save get data as csv
 # Multiprocessing in Python cut time from 150 secs to 22 secs (14x as fast)
-(get_data_csv <- get_data %>%
-  transmute(leader = name,
-            date = paste(date),
-            date_end = paste(date_end),
-            row_no = paste0(row_number(), "/", n())
-            )
+(get_data_geo_csv <- get_data_geo %>%
+    #filter(country %in% c("Georgia", "Zimbabwe", "Mexico")) %>%
+    transmute(leader = name,
+              date = paste(date),
+              date_end = paste(date_end),
+              row_no = paste0(row_number(), "/", n()),
+              geocode
+
+    )
 )
 
-write_csv(get_data_csv, "py/get_data.csv")
+get_data_geo_csv$leader %>% unique
+
+write_csv(get_data_geo_csv, "py/get_data_geo.csv")
 
 # Get tweets ----
 # This stage takes place in Python, outside of R, for now
 # Try to source py script with reticulate in the future to keep pipeline in R
 #use_python("/usr/local/bin/python3", required = TRUE)
-use_virtualenv("~/venv/", required = TRUE)
+#use_virtualenv("~/venv/", required = TRUE)
 
 #source_python("py/get_tweets_multi.py", convert = FALSE)
 
 #py_run_file("py/get_tweets_multi.py")
 
 
-## Bind tweets ----
-# Map across function load all Tweets across hundreds of JSONs
-tweets_raw_list <- list.files("data-raw/tweets/",
-                              full.names = TRUE, recursive = TRUE,
-                              pattern = "_without\\."
-) %>%
-  purrr::map(~read_tweets_back(.))
-
-# Bind tweets into dataframe
-tweets_raw <- bind_raw_tweets(tweets_raw_list)
+## Read tweets ----
+tweets_raw <- read_csv("data-raw/tweets/tweets.csv")
 
 # Save as single object
 #save(tweets_raw, file = "data/tweets_raw.RData")
@@ -88,18 +112,26 @@ tweets_raw <- bind_raw_tweets(tweets_raw_list)
 # Load raw tweets
 #load("data/tweets_raw.RData")
 
-# Format and add variables, including leader and country
-tweets_formatted <- format_tweets(tweets_raw, candidates)
+tweets_raw_small <- tweets_raw[1:300000,]
 
-# Add region names
-tweets_sf <- add_regions(tweets_formatted, boundaries_subnational)
+# Format and add variables
+tweets_formatted <- format_tweets(tweets_raw_small, candidates)
 
-## Add regional covariates to tweets -----
+# Filter out NA leaders and duplicates
+tweets_sub <- filter_tweets(tweets_formatted)
+
+# Add region variables
+tweets_sf <- add_regions(tweets_sub, boundaries_subnational)
+
+# Create tweets tokens
+tweets_tokens <- tweets_sf %>%
+  create_tweet_tokens()
 
 # Save formatted data ----
 save(supp, reign, candidates,
-     elex_master,
+     elex_master, polling,
      boundaries_national, boundaries_subnational,
      tweets_sf,
      senti_lexicons, afinn, afinn_stem,
      file = "data/formatted_data.RData")
+
