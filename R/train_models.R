ad10edee08397fe65a805938e3b46e4e5b6090e6
William Rohde Madsen <wrohdemadsen@icloud.com>
2021-05-05 01:24:07 +0100

    rename

--- R/train_models.R
+++ R/train_models.R
@@ -1,34 +1,34 @@
-# Validation
+# Train models
 
-#' Join election results to raw sentiment estimates
+#' Join targets vector to sentiment estimates
 #' @param raw_pro_shares
-#' @param elex_master
-join_election_to_tweets <- function(senti_per_tweet, elex_master){
+#' @param targets_master
+join_targets_to_senti <- function(df, targets_master){
 
   # Join election results to pro shares
-  senti_per_tweet %>%
+  df %>%
     mutate(region_1 = if_else(is.na(region_1), "National", region_1),
            region_2 = if_else(is.na(region_2), "National", region_2),
-           country = if_else(is.na(country), leader_country, country),
-           id = row_number()
+           country = if_else(is.na(country), leader_country, country)
            ) %>%
     left_join(.,
-              elex_master,
+              targets_master,
               by = c("leader" = "name", "leader_country" = "country", "region_1", "region_2"))
 
 }
 
 
-#' For each tweet, select the nearest election in time
+#' For each tweet, select the nearest target (poll or election) in time
 #' One election per tweet
-select_nearest_election <- function(validate_elex_raw){
+select_nearest_target <- function(df){
 
-  validate_elex_raw %>%
-    group_by(id) %>%
+  df %>%
     # diff between tweet and election date in days
-    mutate(days_diff = (date - elex_date) %>% as.integer,
+    mutate(days_diff = (date - date_target) %>% as.integer,
            days_diff_abs = abs(days_diff)
     ) %>%
+    # choose target for each estimate closest in time
+    group_by(target_id) %>%
     slice_min(days_diff_abs, n = 1) %>%
     ungroup() %>%
     arrange(leader, country, region_1, date)
@@ -38,11 +38,11 @@ select_nearest_election <- function(validate_elex_raw){
 #' Summarise cut-off validation statistics
 #' @param validate_elex_raw
 #' @return summary statistics by cut-off values to evaluate their differences and merits
-summarise_cut_off_validation <- function(validate_elex_raw){
+summarise_cut_off_validation <- function(df){
 
   # Calculate mean, sd and summary of difference between raw estimates and cut-off
   # For period col
-  validate_elex_raw %>%
+  df %>%
     mutate(pro_share_diff = pro_share - votes_share,
            pro_share_roll_diff = pro_share_roll - votes_share) %>%
     # choose which window of days to election to analyse summary for
