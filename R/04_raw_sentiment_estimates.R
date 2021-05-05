3761bdfa47ca0e1a98156e97c65375f6ad2c0344
William Rohde Madsen <wrohdemadsen@icloud.com>
2021-05-05 20:53:37 +0100

    Optimise with data.table

--- R/04_raw_sentiment_estimates.R
+++ R/04_raw_sentiment_estimates.R
@@ -1,36 +1,48 @@
-# Raw sentiment manipulations
+# Raw sentiment estimates
 
 #' Create object with different cut-offs
-#' @param senti_mean
+#' @param senti_tweet
 #' @return senti_cut_offs
-create_cut_offs <- function(senti_per_tweet){
+#' @details data.table does not seem required to cut down on time
+create_cut_offs <- function(senti_tweet){
 
+  # Create vector of different cut offs
   (cut_off_seq <- seq(-2, 2, length.out = 9) %>% round(., 2))
 
-  senti_per_tweet_w_row_no <- senti_per_tweet %>%
+  # Add row number to group by
+  senti_tweet_w_row_no <- senti_tweet %>%
     mutate(row_number = row_number())
 
-  purrr::map_dfr(seq_len(9), function(x) senti_per_tweet_w_row_no) %>%
+  # Add cut off to each tweet
+  purrr::map_dfr(seq_len(9), function(x) senti_tweet_w_row_no) %>%
     group_by(row_number) %>%
-    mutate(cut_off = seq(-2, 2, length.out = 9))
+    mutate(cut_off = seq(-2, 2, length.out = 9)) %>%
+    ungroup() %>%
+    # For each tweet, determine whether it's pro or con based on different cut offs
+    mutate(stance = if_else(afinn_mean > cut_off, "pro", "con"))
+
 
 }
 
-#' Find pro share
+#' Calculate raw estimates
 #' @param senti_cut_offs
 #' @param n_roll window of rolling average
-#' @return pro shares at different cut offs
-find_pro_share <- function(senti_cut_offs, n_roll = 5){
+#' @return raw estimates at different cut offs
+calculate_raw_estimates <- function(senti_cut_offs, n_roll = 5){
 
-  # Create binary stance against cut-off
-  senti_cut_offs %>%
-    mutate(stance = if_else(afinn_mean > cut_off, "pro", "con")) %>%
-    group_by(country, region_1, region_2,
-             leader, leader_country,
-             date, #week = floor_date(date, "week"),
-             cut_off, stance) %>%
-    # number of pros and cons per day
-    summarise(n = n()) %>%
+  # Calculate raw number of pros and cons per day, by cut off
+  senti_cut_offs_dt <- as.data.table(senti_cut_offs)
+
+  raw_n_per_day <- senti_cut_offs_dt[, by = list(country, region_1, region_2,
+                                                 leader, leader_country,
+                                                 date, #week = floor_date(date, "week"),
+                                                 cut_off, stance),
+                                     .(n = .N)]
+
+
+
+  # number of pros and cons per day
+  raw_n_per_day %>%
     pivot_wider(names_from = stance, values_from = n) %>%
     group_by(country, leader, cut_off) %>%
     arrange(date) %>%
