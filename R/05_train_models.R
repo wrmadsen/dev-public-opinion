6b25724f5242b64f404493f0bb91b80cf07fa870
William Rohde Madsen <wrohdemadsen@icloud.com>
2021-05-06 14:38:39 +0100

    fix issue with joining targets to region

--- R/05_train_models.R
+++ R/05_train_models.R
@@ -3,12 +3,12 @@
 #' Join targets vector to sentiment estimates
 #' @param raw_pro_shares
 #' @param targets_master
-join_targets_to_senti <- function(df, targets_master){
+join_targets <- function(df, targets_master){
 
   # Join election results to pro shares
   df %>%
     mutate(region_1 = if_else(is.na(region_1), "National", region_1),
-           region_2 = if_else(is.na(region_2), "National", region_2),
+           region_2 = if_else(is.na(region_2), region_1, region_2),
            country = if_else(is.na(country), leader_country, country)
            ) %>%
     left_join(.,
@@ -20,9 +20,10 @@ join_targets_to_senti <- function(df, targets_master){
 
 #' For each tweet, select the nearest target (poll or election) in time
 #' One election per tweet
-select_nearest_target <- function(df){
+select_nearest_target <- function(senti_targets_all){
 
-  df %>%
+  # Select nearest election or poll for each sentiment day
+  senti_targets <- senti_targets_all %>%
     # diff between tweet and election date in days
     mutate(days_diff = (date - date_target) %>% as.integer,
            days_diff_abs = abs(days_diff)
@@ -32,6 +33,17 @@ select_nearest_target <- function(df){
     slice_min(days_diff_abs, n = 1) %>%
     ungroup() %>%
     arrange(leader, country, region_1, date)
+
+  # Return
+  senti_targets
+
+}
+
+#' Add GDL covariates
+add_gdl_covariates <- function(senti_targets, gdl_interpo){
+
+  left_join(senti_targets, gdl_interpo,
+            by = c("year", "country", "region_1" = "gadm_region"))
 
 }
 
