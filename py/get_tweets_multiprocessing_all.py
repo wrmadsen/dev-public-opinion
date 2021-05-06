09a9de81dc9957263031acce9fe7eaebb5dafe6f
William Rohde Madsen <wrohdemadsen@icloud.com>
2021-05-06 20:36:04 +0100

    rename py scripts, split into all or geo

--- py/get_tweets_multiprocessing_all.py
+++ py/get_tweets_multiprocessing_all.py
@@ -4,7 +4,7 @@ import time
 from multiprocessing.dummy import Pool as ThreadPool
 
 # Load data
-get_data = pd.read_csv("py/get_data.csv")
+get_data = pd.read_csv("py/get_data_all.csv")
 
 # Set up twint function
 def get_tweets(search, since, until, row_no):
@@ -12,13 +12,10 @@ def get_tweets(search, since, until, row_no):
     c = twint.Config()
     c.Search = search
     c.Lang = "en"
-    c.Limit = 5000
-    #c.Geo = geo
+    c.Limit = 50000
     c.Since = since
     c.Until = until
-    #c.User_full = True
     c.Store_csv = True
-    #c.Store_json = True
     c.Output = "data-raw/tweets/tweets.csv"
     c.Hide_output = True
 
---
+++ py/get_tweets_multiprocessing_geo.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import twint
+import time
+from multiprocessing.dummy import Pool as ThreadPool
+
+# Load data
+get_data = pd.read_csv("py/get_data_geo.csv")
+
+# Set up twint function
+def get_tweets(search, since, until, row_no, geocode):
+
+    c = twint.Config()
+    c.Search = search
+    c.Lang = "en"
+    c.Limit = 100000
+    c.Geo = geocode
+    c.Since = since
+    c.Until = until
+    c.Store_csv = True
+    c.Output = "data-raw/tweets/tweets.csv"
+    c.Hide_output = True
+
+    twint.run.Search(c)
+
+    print("row " + row_no)
+
+    time.sleep(2)
+
+
+start = time.perf_counter()
+
+# Convert dataframe to tuples
+records = get_data.to_records(index=False)
+tuples = list(records)
+
+# Get tweets in parallel
+pool = ThreadPool(10)
+pool.starmap(get_tweets, tuples)
+pool.close()
+pool.join()
+
+finish = time.perf_counter()
+
+print(f'Finished in {round(finish-start, 2)} second(s)')
